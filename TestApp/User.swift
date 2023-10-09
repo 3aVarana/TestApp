34ddc9762dab31de00487000e3d21894b8c86d05
Andres Arana <aarana@ecisolutions.com>
2023-10-09 16:22:17 -0400

    create bae User struct

---
+++ TestApp/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  TestApp
+//
+//  Created by Admin on 10/9/23.
+//
+
+import Foundation
+
+struct User {
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var emai: String
+    var gender: String
+    var pictureURL: String
+    var phone: String
+    var birthday: String
+    var twitterHandle: String
+}
